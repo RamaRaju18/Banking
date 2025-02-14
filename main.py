23c03f0dca21ecdf123f102dc2a7c702d292154b
Rama Raju <guntururamaraju@gmail.com>
2025-02-14 15:50:07 +0530

    Banking

---
+++ main.py
@@ -0,0 +1,59 @@
+def account_no():
+    acc = ("******")
+    user = (int(input("Enter Account Number:")))
+    Acc = str(user)
+    if len(Acc) == len(acc):
+        return user
+    else:
+        print("Enter Valid Account Number")
+        return account_no()
+
+def show_balance():
+    print(f"Your Balance Is: Rs {balance:.2f}")
+    
+def deposit():
+    amount = float(input("Enter Your Amount:"))
+
+    if amount < 0:
+        print("Enter Valid Amount:")
+        return 0
+
+    else:
+        return amount
+    
+def withdraw():
+    amount = float(input("Enter Your Amount:"))
+    if amount > balance:
+        print("Insufficient Funds")
+        return 0
+    elif amount < 0 :
+        print("Enter Valid Amount")
+        return 0
+    else:
+        return amount
+balance = 0
+Banking = True
+print(account_no())
+while Banking:
+        print("Banking")
+        print("1.Balance Enquiry")
+        print("2.Deposit")
+        print("3.Withdraw")
+        print("4.Exit")
+
+        Choice = int(input("Enter Your Number:"))
+
+        if Choice == 1:
+            balance = show_balance()
+        elif Choice == 2:
+            balance += deposit()
+        elif Choice == 3:
+            balance -= withdraw()
+        elif Choice == 4:
+            Banking = False
+        else:
+            print("Enter valid Number")
+
+
+
+print("Thank You For Banking With Us")
