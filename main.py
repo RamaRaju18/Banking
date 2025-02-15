918a8a83e60d291469d58e4b3ca30e26dbd1901e
Rama Raju <guntururamaraju@gmail.com>
2025-02-15 11:11:59 +0530

    Banking Program update

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 def account_no():
     acc = ("******")
-    user = (int(input("Enter Account Number:")))
+    user = (int(input("Enter  Your 6 Digit Account Number:")))
     Acc = str(user)
     if len(Acc) == len(acc):
         return user
     else:
         print("Enter Valid Account Number")
         return account_no()
-
 def show_balance():
     print(f"Your Balance Is: Rs {balance:.2f}")
-    
+    return balance
 def deposit():
     amount = float(input("Enter Your Amount:"))
 
     if amount < 0:
         print("Enter Valid Amount:")
         return 0
-
     else:
+        print("Your Deposit Is Successfull")
         return amount
-    
 def withdraw():
     amount = float(input("Enter Your Amount:"))
     if amount > balance:
@@ -30,6 +28,7 @@ def withdraw():
         print("Enter Valid Amount")
         return 0
     else:
+        print("Your Withdrawl Is Successfull")
         return amount
 balance = 0
 Banking = True
@@ -40,9 +39,7 @@ while Banking:
         print("2.Deposit")
         print("3.Withdraw")
         print("4.Exit")
-
         Choice = int(input("Enter Your Number:"))
-
         if Choice == 1:
             balance = show_balance()
         elif Choice == 2:
@@ -53,7 +50,4 @@ while Banking:
             Banking = False
         else:
             print("Enter valid Number")
-
-
-
 print("Thank You For Banking With Us")
